41ab02aab607c45dd09eaf5b6fbf620c570a57c9
Yvesena <yvesribeiro51@gmail.com>
2024-08-22 13:57:54 -0300

    criou a classe Descriçao

--- crudLp1/include/Descricao.h
+++ crudLp1/include/Descricao.h
@@ -1,15 +1,32 @@
 #ifndef DESCRICAO_H
 #define DESCRICAO_H
+#include <bits/stdc++.h>
 
+using namespace std;
 
 class Descricao
 {
     public:
         Descricao();
+        Descricao(float peso, string data_de_lancamento, string fabricante,float potencia);
 
-    protected:
+        float getpeso();
+        string getdatadelancamento();
+        string getfabricante();
+        float getpotencia();
+
+        void setpotencia(float potencia);
+        void setpeso(float peso);
+        void setdatadelancamento(string data_de_lancamento);
+        void setfabricante(string fabricante);
+
+
 
     private:
+        float potencia;
+        float peso;
+        string data_de_lancamento;
+        string fabricante;
 };
 
 #endif // DESCRICAO_H
--- crudLp1/src/Descricao.cpp
+++ crudLp1/src/Descricao.cpp
@@ -2,5 +2,40 @@
 
 Descricao::Descricao()
 {
-    //ctor
+
+}
+Descricao::Descricao(float peso, string data_de_lancamento, string fabricante, float potencia){
+    this->potencia = potencia;
+    this->peso=peso;
+    this->data_de_lancamento=data_de_lancamento;
+    this->fabricante=fabricante;
+}
+
+//get
+float Descricao::getpeso(){
+   return this->peso;
+}
+string Descricao::getdatadelancamento(){
+ return this->data_de_lancamento;
+}
+string Descricao::getfabricante(){
+ return this->fabricante;
+}
+float Descricao::getpotencia(){
+   return this->potencia;
+}
+
+
+//set
+void Descricao::setpeso(float peso){
+    this->peso = peso;
+}
+void Descricao::setdatadelancamento(string data_de_lancamento){
+  this->data_de_lancamento = data_de_lancamento;
+}
+void Descricao::setfabricante(string fabricante){
+   this->fabrincante = fabricante;
+}
+void Descricao::setpotencia(){
+ this->potencia=potencia;
 }
